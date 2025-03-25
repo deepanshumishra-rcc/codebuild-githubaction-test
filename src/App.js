d27996ce60b6f8bb173bc0490a4e0601416b2fa6
Deepanshu Mishra <deepanshumishra@Deepanshus-MacBook-Pro.local>
2025-03-25 16:31:54 -0700

    Custom appp ..

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Welcome to My Custom React App!</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi! There
         </p>
         <a
           className="App-link"
@@ -21,5 +20,6 @@ function App() {
     </div>
   );
 }
+
 
 export default App;
