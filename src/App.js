9d87474488ad2f96ed2be3240d4a0a8b51d769b3
Deepanshu Mishra <deepanshumishra@Deepanshus-MacBook-Pro.local>
2025-03-25 17:34:37 -0700

    app version 3..

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
       <header className="App-header">
         <h1>Welcome to My Custom React App!</h1>
         <p>
-          Hi! There...
+          Hi! There... this is app version v3
         </p>
         <a
           className="App-link"
